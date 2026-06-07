b973317e551fed70b084ebd3a52cc117743c6518
Anass <404soul2006@gmail.com>
2026-06-07 11:35:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,12 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -49,7 +50,7 @@ const App = () => {
       setIsLoading(false);
     }
   };
-  useEffect(() => { fetchMovies(); }, []);
+  useEffect(() => { fetchMovies(searchTerm); }, [searchTerm]);
   return (
     <main>
       <div className="pattern" />
